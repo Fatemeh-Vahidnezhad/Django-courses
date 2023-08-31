789c677eb3ee9ee444590496116fb1dabf23d029
Fatemeh-Vahidnezhad <Fatemeh.Vahidnezhad@gmail.com>
2023-08-31 08:52:48 -0700

    test for account & pages

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class SignUpTest(TestCase):
+    username = 'myusername'
+    email = 'myusername@gmail.com'
+
+    def test_url_signup(self):
+        request = self.client.get(reverse('signup'))
+        self.assertEqual(request.status_code, 200)
+
+    def test_name_signup(self):
+        request = self.client.get('/accounts/signup/')
+        self.assertEqual(request.status_code, 200)
+
+    def test_create_form_signup(self):
+        user = get_user_model().objects.create_user(
+            self.username,
+            self.email,
+
+        )
+        self.assertEqual(get_user_model().objects.all()[0].username, 'myusername')
+        self.assertEqual(get_user_model().objects.all()[0].email, 'myusername@gmail.com')
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+
+
+
+
+
+
+
+
+
--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_name_home_page(self):
+        request = self.client.get(reverse('home'))
+        self.assertEqual(request.status_code, 200)
+
+    def test_url_home_page(self):
+        request = self.client.get('/')
+        self.assertEqual(request.status_code, 200)
+
+    def test_content_home_page(self):
+        request = self.client.get('/')
+        self.assertContains(request, 'home')
+
+
+
