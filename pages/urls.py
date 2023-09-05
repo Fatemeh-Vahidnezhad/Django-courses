e694c1d69e9333f83f9fee4b5f41858428a37694
Fatemeh-Vahidnezhad <Fatemeh.Vahidnezhad@gmail.com>
2023-09-05 09:48:52 -0700

    add about us and contact us pages

--- pages/urls.py
+++ pages/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('contact/', views.ContactView, name='contactus'),
+    path('about/', views.aboutView, name='aboutus'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,11 @@ from django.views.generic.base import TemplateView
 
 class HomePageView (TemplateView):
     template_name = 'home.html'
+
+
+def ContactView(request):
+    return render(request, template_name='pages/contact.html')
+
+
+def aboutView(request):
+    return render(request, template_name='pages/aboutus.html')
