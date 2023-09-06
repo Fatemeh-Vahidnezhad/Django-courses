c3923ea4a244d326008d1367ef161312ac23265d
Fatemeh-Vahidnezhad <Fatemeh.Vahidnezhad@gmail.com>
2023-09-06 02:49:23 -0700

    create, update and delete views

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from . import views
 
 urlpatterns = [
-    path('', BookListView.as_view(), name='book_list'),
-    path('<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('', views.BookListView.as_view(), name='book_list'),
+    path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('create/', views.CreateBookView.as_view(), name='create_book'),
+    path('<int:pk>/edit/', views.UpdateBookView.as_view(), name='update_book'),
+    path('<int:pk>/delete/', views.DeleteBookView.as_view(), name='delete_book'),
 
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Books
 
@@ -16,3 +17,20 @@ class BookDetailView(generic.DetailView):
     context_object_name = 'book'
 
 
+class CreateBookView(generic.CreateView):
+    model = Books
+    template_name = 'books/create_books.html'
+    fields = ('title', 'author', 'pages', 'description',)
+
+
+class UpdateBookView(generic.UpdateView):
+    model = Books
+    template_name = 'books/update_book.html'
+    fields = ('title', 'author', 'pages', 'description',)
+
+
+class DeleteBookView(generic.DeleteView):
+    model = Books
+    template_name = 'books/delete_book.html'
+    context_object_name = 'book'
+    success_url = reverse_lazy('book_list')
